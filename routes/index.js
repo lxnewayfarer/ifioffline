bfad9e86ce78abe125126193ca75d2e99970a230
lxne <lxnewayfarer@yandex.ru>
2020-08-31 03:33:26 +0700

    express 4

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express'),
   router = express.Router()
+const db = require('../models')
+
 
 router.route('/').get((req, res) => {
-  res.send('API index get')
+    db.Users.findAll()
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
 })
 
 module.exports = router
