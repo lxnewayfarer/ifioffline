3c676cb2401368e968de5dc8ca64b7d440af35f6
lxne <lxnewayfarer@yandex.ru>
2020-08-31 03:22:47 +0700

    /api basic GET added

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express'),
   router = express.Router()
 
 router.route('/').get((req, res) => {
-  res.send('pdji')
+  res.send('API index get')
 })
 
 module.exports = router
