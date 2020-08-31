4bb985c19bb15f31d75698adb64c802ddebcdd56
lxne <lxnewayfarer@yandex.ru>
2020-09-01 01:45:04 +0700

    added test data

--- models/index.js
+++ models/index.js
@@ -22,6 +22,10 @@ db.UserDelay = require('./UserDelay.js')(sequelize, Sequelize)
 db.Users.hasMany(db.TrackedServices, { foreignKey: 'id_user', sourceKey: 'id' })
 db.TrackedServices.belongsTo(db.Users, { foreignKey: 'id_user', targetKey: 'id' })
 
+db.Services.hasMany(db.TrackedServices, { foreignKey: 'id_service', sourceKey: 'id' })
+db.TrackedServices.belongsTo(db.Services, { foreignKey: 'id_service', targetKey: 'id' })
+
+
 db.Users.hasMany(db.Recievers, { foreignKey: 'id_user', sourceKey: 'id' })
 db.Recievers.belongsTo(db.Users, { foreignKey: 'id_user', targetKey: 'id' })
 
--- models/seed.js
+++ models/seed.js
@@ -1,5 +1,6 @@
 const db = require('.')
 
+// test data for database
 async function start () {
   await db.sequelize.sync({ force: true }).then(() => {
     console.log('Drop and re-sync db.')
@@ -12,5 +13,17 @@ async function start () {
   db.Services.create({ name: 'VK', link: 'vk.com', isCheckable: true, isSendable: false })
   db.Services.create({ name: 'Instagram', link: 'instagram.com', isCheckable: true, isSendable: false })
   db.Services.create({ name: 'Facebook', link: 'facebook.com', isCheckable: true, isSendable: false })
+
+  db.TrackedServices.create({ identifier: 'lxnewayfarer', id_user: 3, id_service: 1})
+  db.TrackedServices.create({ identifier: 'lxnewayfarer', id_user: 1, id_service: 3})
+  db.TrackedServices.create({ identifier: 'lxnewayfarer', id_user: 2, id_service: 2})
+
+  db.Recievers.create({ identifier: 'lxnewayfarer', message: '111Hello! How are you111', id_user: 1, id_service: 1 })
+  db.Recievers.create({ identifier: 'lxnewayfarer', message: '222Hello! How are you222', id_user: 2, id_service: 2 })
+  db.Recievers.create({ identifier: 'lxnewayfarer', message: '333Hello! How are you222', id_user: 3, id_service: 3 })
+
+  db.UserDelay.create({ value: 5, id_user: 1 })
+  db.UserDelay.create({ value: 10, id_user: 3 })
+  db.UserDelay.create({ value: 15, id_user: 2 })
 }
 start()
